f17b2b6e95f765142fbe2381b0c60abf06babbe5
Fellipe Corominas <lefelps@hotmail.com>
2022-06-13 18:57:13 -0300

    the dungeon -> opponents form

--- src/components/dungeon/form.jsx
+++ src/components/dungeon/form.jsx
@@ -5,7 +5,54 @@ function OpponentForm() {
     return (
         <div className="content">
             <div className="form-container">
-                <div className="row push-right">
+                <div>
+                    <div className="form-title">New Opponent</div>
+                    <div className="input-section">
+                        <div className="input-row">
+                            <div className='input-group-50'>
+                                <label htmlFor="username">Name</label>
+                                <input type="text" className='input-field'
+                                // onChange={(e) => {
+                                //     setUserData({ ...userData, username: e.target.value })
+                                // }} value={userData.username}
+                                />
+                            </div>
+                        </div>
+                        <div className="input-row">
+                            <div className='input-group-50'>
+                                <label htmlFor="username">Difficulty</label>
+                                <select className='input-field'
+                                // onChange={(e) => {
+                                //     setUserData({ ...userData, username: e.target.value })
+                                // }} value={userData.username}
+                                >
+                                    <option value="">Select...</option>
+                                </select>
+                            </div>
+                            <div className='input-group-50'>
+                                <label htmlFor="username">Personality</label>
+                                <select className='input-field'
+                                // onChange={(e) => {
+                                //     setUserData({ ...userData, username: e.target.value })
+                                // }} value={userData.username}
+                                >
+                                    <option value="">Select...</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="input-row">
+                            <div className='input-group'>
+                                <label htmlFor="username">About</label>
+                                <textarea type="text" className='input-field textarea'
+                                // onChange={(e) => {
+                                //     setUserData({ ...userData, username: e.target.value })
+                                // }} value={userData.username}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row push-right gap-25">
                     <button className="button-rounded gray text-white ">
                         Cancel
                     </button>
