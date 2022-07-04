982baf3dd5b876860accdbe3dafefdee9864d947
Fellipe Corominas <lefelps@hotmail.com>
2022-07-04 19:17:53 -0300

    icon filter adjustments

--- src/components/challenges/form.jsx
+++ src/components/challenges/form.jsx
@@ -22,17 +22,18 @@ function ChallengeForm() {
 
     const [iconSearch, setIconSearch] = useState("")
 
-    const [iconPage, setIconPage] = useState({
-        active: 0,
-        total: 0,
-    })
+    const [iconMaxValues, setIconMaxValues] = useState(48)
 
     function searchMatch(search, array) {
         const matches = array.filter(key => {
             var regexp = new RegExp(search, "gi")
             return key.match(regexp)
         })
-        return matches
+
+        let maxValues
+        if (matches.length > iconMaxValues) { maxValues = iconMaxValues } else { maxValues = matches.length }
+        let arrayValues = matches.slice(0, maxValues)
+        return arrayValues
     }
 
     useEffect(() => {
@@ -117,7 +118,7 @@ function ChallengeForm() {
                                     <FontAwesomeIcon className="text-light text-huge to-right" icon={fa.faMagnifyingGlass} />
                                 </div>
                             </div>
-                            <div className="row gap-15 wrap maxh-200 overflowy-scroll p-15 filled-container rounded-15">
+                            <div className="row gap-15 wrap maxh-200 minh-20 overflowy-scroll p-15 filled-container rounded-15">
                                 {searchMatch(iconSearch, Object.keys(fa).filter(i => i !== 'fas' && i !== 'prefix')).map((key, index) => (
                                     <div key={index} className={`${challenge.icon === key ? ' blue text-white ' : ' white text-dark border-light '} pointer p-5 square-small centered vertical-center`}>
                                         <FontAwesomeIcon icon={fa[key]} size="2x"
@@ -136,6 +137,11 @@ function ChallengeForm() {
                                             <b>{challenge.icon}</b>
                                         </>
                                         : <span>None</span>}
+                                </div>
+                                <div className="row to-right gap-15">
+                                    <b>Showing</b>
+                                    <input type="number" className='text-blue naked w-75px text-center text-big' value={iconMaxValues} onChange={(e) => setIconMaxValues(e.target.value)}/>
+                                    <b>Results</b>
                                 </div>
                             </div>
                         </div>
